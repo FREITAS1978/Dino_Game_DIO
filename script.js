f6ecdab5a0de0db260db808c1a1da48b939135e4
FREITAS1978 <fabio1554@hotmail.com>
2021-11-01 11:11:39 -0300

    FREITAS1978

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const dino = document.querySelector('.dino');
 const background = document.querySelector('.background');
 
 let isJumping = false;
+let isGameOver = false;
+let position = 0;
 
 function handleKeyUp(event) {
      if (event.keycode === 32) {
@@ -10,14 +12,14 @@ function handleKeyUp(event) {
           }
      }
 }
-
-function jump(){
-     let position = 0;
+function jump() {
      isJumping = true;
 
      let upInterval = setInterval (() => {
           if (position >= 150){
+                //DESCENDO
                clearInterval(upInterval);
+              
                let downInterval = setInterval(() => {
                     if (position <= 0) {
                          clearInterval(downInterval);
@@ -28,6 +30,7 @@ function jump(){
                     }
                }, 20);
           } else {
+               //SUBINDO
                position += 20;
                dino.style.bottom = position + 'px';
           }
@@ -37,10 +40,30 @@ function jump(){
 function createCactus() {
      const cactus = document.createElement('div');
      let cactusPosition = 1000;
-     
+     let randomTime = Math.random() *6000;
+
+     if (isGameOver) return;
+
      cactus.classList.add('cactus');
-     cactus.sryle.left = 1000 + 'px';
      background.appendChild(cactus);
+     cactus.style.left = cactusPosition + 'px';
+     
+     let leftTimer = setInterval(() => {
+          if (cactusPosition < -60) {
+               //SAIU DA TELA
+               clearInterval (leftTimer);
+               background.removeChild (cactus);
+          } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+               //GAMEOVER
+               clearInterval (leftTimer);
+               isGameOver = true;
+               document.body.innerHTML = '<h1 class = "game-over">Fim de Jogo!</h1>';
+          }else {
+               cactusPosition -= 10;
+               cactus.style.left = cactusPosition + 'px';
+          }
+     }, 20);
+setTimeout (createCactus, randomTime);
 }
 
 createCactus();
